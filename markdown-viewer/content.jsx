1dfddabbc5cefb6984a4011af4fd6b3f58397e45
Bjarke Walling <bwp@bwp.dk>
2020-10-29 15:52:50 +0100

    Improve doc. Adding Troels todo.

--- markdown-viewer/content.jsx
+++ markdown-viewer/content.jsx
@@ -1,7 +1,9 @@
 import { readFileSync } from "fs";
 
-//-- We use readFileSync with static strings in order to track the files automatically --
-// Whenever a markdown file is updated, Parcel sends an update using hot reload to the browser.
+//-- Directory/file tree structure of markdown files --
+// We use readFileSync with static strings in order to track the files automatically.
+// Whenever a file is updated, Parcel should send a hot reload update to the browser.
+// Please try to follow the directory structure of the repo.
 const content = {
   documents: {
     "data-models.md": readFileSync("../documents/data-models.md", "utf8"),
@@ -9,6 +11,9 @@ const content = {
   },
   "markdown-viewer": {
     "readme.md": readFileSync("../markdown-viewer/readme.md", "utf8"),
+  },
+  planning: {
+    "todo-troels.txt": readFileSync("../planning/todo-troels.txt", "utf8"),
   },
   "readme.md": readFileSync("../readme.md", "utf8"),
 };
