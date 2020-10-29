3a131def17d2ca509b684110f9538f5b822ffa5d
troels <fqx851@alumni.ku.dk>
2020-10-29 17:13:26 +0100

    Look up market instead of hard code in Plot spike

--- spikes/troels/plot.py
+++ spikes/troels/plot.py
@@ -3,12 +3,14 @@ from matplotlib import pyplot as pl
 import numpy as np
 
 table = pq.read_table("../../data/2020_kraken.parquet")
-i = 10
+
+markets = table.schema.metadata[b'markets'].decode().split(',')
+market_id = markets.index('kraken:btc/eur')
+
+i = market_id
 t = table["time"].chunk(i)
 p = table["price"].chunk(i)
 s = table["price_scale"].chunk(i).to_numpy().astype(np.int8)
-
-print(t)
 
 pl.plot(t, p.to_numpy()*10.0**(-s))
 pl.show()
