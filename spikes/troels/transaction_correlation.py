7269fc2559885a0ad017db758bef4fd913a1e6ee
troels <fqx851@alumni.ku.dk>
2020-11-03 10:21:47 +0100

    Add scatter plot of previous and next transaction
    - to visually inspect for correlations

---
+++ spikes/troels/transaction_correlation.py
@@ -0,0 +1,24 @@
+import pyarrow.parquet as pq
+import numpy as np
+from matplotlib import pyplot as pl
+from decimal import Decimal
+from datetime import timedelta
+
+
+table = pq.read_table("../../data/2020_kraken.parquet")
+markets = table.schema.metadata[b"markets"].decode().split(",")
+market_id = markets.index("kraken:btc/eur")
+
+t = table["time"].chunk(market_id).to_numpy()
+p = table["price"].chunk(market_id).to_numpy()
+s = table["price_scale"].chunk(market_id).to_numpy().astype(np.int8)
+
+# Apply scale to get price as floating point number
+p = p * 10.0 ** (-s)
+
+# Find changes in price
+dp = p[:-1] - p[1:]
+
+# Plot previous transaction against current
+pl.scatter(dp[:-1], dp[1:])
+pl.show()
