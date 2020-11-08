7d84d7bba6cae4ea30c781b90274431c4272bd94
Bjarke Walling <bwp@bwp.dk>
2020-11-08 12:18:15 +0100

    Fixing testing issue.

--- lib/dataset/__main__.py
+++ lib/dataset/__main__.py
@@ -2,14 +2,15 @@ from .cli import main
 import traceback
 import sys
 
-try:
-    main(args=sys.argv[1:])
-except KeyboardInterrupt:
-    print("Ctrl-C, exiting ...")
-    exit(0)
-except SystemExit:
-    raise
-except:
-    print("-- error: --", file=sys.stderr)
-    traceback.print_exc()
-    exit(1)
+if __name__ == "__main__":
+    try:
+        main(args=sys.argv[1:])
+    except KeyboardInterrupt:
+        print("Ctrl-C, exiting ...")
+        exit(0)
+    except SystemExit:
+        raise
+    except:
+        print("-- error: --", file=sys.stderr)
+        traceback.print_exc()
+        exit(1)
