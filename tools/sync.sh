4f8645708e447411a4d360aa1ed1db4ebab0f997
Bjarke Walling <bwp@bwp.dk>
2020-11-15 16:53:36 +0100

    Removing Python sync script (has issues). Adding new sync script using rsync.

---
+++ tools/sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Immediately exit on failure; work in root directory
+set -e
+cd $(dirname "$0")/..
+
+# Sync files using rsync
+rsync -chavzP --delete --stats trading-rsync@herkules:data/trades ./data/
