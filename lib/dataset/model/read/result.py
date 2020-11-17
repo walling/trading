d5aa70d1d8aede8310d4d127c95c482895db361c
Bjarke Walling <bwp@bwp.dk>
2020-11-17 17:38:45 +0100

    After resyncing data dir, this should work.

--- lib/dataset/model/read/result.py
+++ lib/dataset/model/read/result.py
@@ -1,11 +1,14 @@
 from typing import List, Optional
 from pathlib import Path
 import pyarrow.dataset as ds
+import pyarrow as pa
 import numpy as np
 
 # Trades data location.
 ROOT_DIR = (Path(__file__).parent / ".." / ".." / ".." / "..").resolve()
 DATA_DIR = ROOT_DIR / "data"
+
+PARTITIONING = ds.partitioning(field_names=["source", "exchange", "instrument", "year"])
 
 
 class Result:
@@ -18,7 +21,11 @@ class Result:
         self._subject = subject
         self._columns = columns
         self._path = DATA_DIR / subject
-        self._dataset = ds.dataset(self._path, format="parquet", partitioning="hive")
+        self._dataset = ds.dataset(
+            self._path,
+            format="parquet",
+            partitioning=PARTITIONING,
+        )
         self._table = self._dataset.to_table(filter=dataset_filter, columns=columns)
 
     @property
@@ -33,27 +40,23 @@ class Result:
     def num_rows(self):
         return len(self._table)
 
-    def raw_table_pandas(self):
-        return self._table.to_pandas()
-
-    def raw_table_numpy(self):
-        return self._table.to_numpy()
-
-    def raw_table_arrow(self):
-        return self._table
-
     def column_numpy(self, name, decimals_as_floats=False, timestamps_as_floats=False):
-        values = self._table[name].to_numpy()
+        field = self._table.schema.field(name)
 
-        metadata = self._table.schema.field(name).metadata
-        if decimals_as_floats and metadata and b"scale" in metadata:
-            scale = int(metadata[b"scale"].decode())
-            return values / (10.0 ** scale)
+        if isinstance(field.type, pa.TimestampType):
+            if timestamps_as_floats:
+                return self._table[name].cast(pa.float64).to_numpy() / 1e9
+            else:
+                return self._table[name].to_numpy()
 
-        if timestamps_as_floats and np.issubdtype(values.dtype, np.datetime64):
-            return values.astype(np.float64) / 1e9
+        if isinstance(field.type, pa.StructType):
+            series = self._table[name].flatten()
+            if decimals_as_floats:
+                return series[0].to_numpy() / (10.0 ** series[1].to_numpy())
+            else:
+                return series[0].to_numpy()
 
-        return values
+        return self._table[name].to_numpy()
 
     def __len__(self) -> int:
         return self.num_rows
